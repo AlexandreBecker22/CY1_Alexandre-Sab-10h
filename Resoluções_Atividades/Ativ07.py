6301c2bb1edbda2009500bc5c3e10605da277483
Alexandre B. de S. L. Ferreira <af22052013@gmail.com>
2025-09-20 11:03:05 -0300

    Começo de resolução

---
+++ Resoluções_Atividades/Ativ07.py
@@ -0,0 +1,12 @@
+# 1-
+
+c = input("Digite uma cidade: ")
+p = input("Digite o país da cidade: ")
+
+def cidadeEPaís(_c, _p="Brasil"):
+    print(f"{c} está localizado no país {p}.")
+
+cidadeEPaís(c,p)
+
+# 2-
+
