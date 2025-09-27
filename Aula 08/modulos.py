0c66a000b677239911f700a235de1499a2a418ce
Alexandre B. de S. L. Ferreira <af22052013@gmail.com>
2025-09-27 10:42:58 -0300

    meio da aula 8

---
+++ Aula 08/modulos.py
@@ -0,0 +1,5 @@
+import math
+
+print(dir(math))# mostra o que tem na biblioteca ( no caso é 'math')
+print(help(math.floor))# Explica uma função( no caso é 'floor') dentro da biblioteca( no caso é 'math')
+
