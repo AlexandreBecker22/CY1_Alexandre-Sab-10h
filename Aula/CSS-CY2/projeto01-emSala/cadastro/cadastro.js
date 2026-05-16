94c5dd74c18cd0421fde0e8aa48ba76e97297dea
Alexandre B. de S. L. Ferreira <af22052013@gmail.com>
2026-05-16 12:05:09 -0300

    22

---
+++ Aula/CSS-CY2/projeto01-emSala/cadastro/cadastro.js
@@ -0,0 +1,78 @@
+let IP01 = document.getElementById("IP01");
+
+// document → busca no HTML
+// .getElementById(" ID ") → elemento com o ID x
+// .querySelector("# ID") → elemento com o ID x
+// .querySelector(". CLASS") → elemento com o CLASS x 
+
+let IP02 = document.querySelector(".IP02")
+let IP03 = document.querySelector(".IP03")
+let link = document.querySelector("#link")
+let IMG01 = document.querySelector("#IMG01")
+let lista_de_música = document.querySelector(".lista_de_música");
+let musicas = [];
+
+const musica = (IP01, IP02, IP03, link, IMG01) => ({
+    IP01,
+    IP02,
+    IP03, 
+    link, 
+    IMG01
+
+})
+
+function Pegar_img() {
+    const urlIMG = URL.createObjectURL(IMG01.files[0]);
+    return urlIMG;
+}
+
+function Cadastrar_Jogo() {
+    const urlImagem = Pegar_img();
+    const nova_musica = musica(IP01.value, IP02.value, IP03.value, link.value,  urlImagem);
+    musicas.push(nova_musica);
+
+
+    // lista_de_música.innerHTML = "";
+    // for (let i = 0; i < musicas.length; i ++){
+    //     lista_de_música.innerHTML += "" + musicas[i];
+    // }
+
+    for (let i = 0; i < musicas.length; i++) {
+        
+        const div = document.createElement("div")
+        div.classList.add("div01");
+        div.innerHTML = `
+        <h3 class="h3h401" class="div01-C" id= "H3-01"> ${musicas[i].IP01}  </h3>
+        <h4 class="h3h401" class="div01-C id= "H4-01"> ${musicas[i].IP02}  </h4>
+        <h4 class="h3h401" class="div01-C id= "H4-02"> ${musicas[i].IP03}  </h4>
+        <a class="h3h401" class="div01-C id="alink01" href="${musicas[i].link}"> Link da música </a>
+        <img class="div01-C" id="imgIMG01"src= "${musicas[i].IMG01}">
+        <button class="B02" onclick="Remover_Elementos(${i})">Remover Música</button>
+        `
+        lista_de_música.appendChild(div);
+
+    }
+
+}
+
+function Remover_Elementos(posicao_Array) {
+   musicas.splice(posicao_Array,1);
+
+  for (let i = 0; i < musicas.length; i++) {
+        
+        const div = document.createElement("div")
+        div.classList.add("div01");
+        div.innerHTML = `
+        <h3 class="h3h401" class="div01-C" id= "H3-01"> ${musicas[i].IP01}  </h3>
+        <h4 class="h3h401" class="div01-C id= "H4-01"> ${musicas[i].IP02}  </h4>
+        <h4 class="h3h401" class="div01-C id= "H4-02"> ${musicas[i].IP03}  </h4>
+        <a class="h3h401" class="div01-C id="alink01" href="${musicas[i].link}"> Link da música </a>
+        <img class="div01-C" id="imgIMG01"src= "${musicas[i].IMG01}">
+        <button class="B02" onclick="Remover_Elementos(${i})">Remover Música</button>
+        `
+        lista_de_música.appendChild(div);
+
+    }
+
+}
+
