d1f16062d8228fb81893573808293f142af342a8
Alexandre B. de S. L. Ferreira <af22052013@gmail.com>
2026-09-12 12:01:25 -0300

    Aula 4

--- Biblioteca_de_jogos-CY2/Java/cadastro.js
+++ Biblioteca_de_jogos-CY2/Java/cadastro.js
@@ -1,19 +1,68 @@
 let usu = document.querySelector(".usuario01");
 let sen = document.querySelector(".senha01");
 let dat = document.querySelector(".data01");
+let mail = document.querySelector(".email01");
 let usus = [];
 
 const USU = (
     nomeExterno,
     senhaExterna,
     dataExterna,
-) => ({nome: nomeExterno, senha: senhaExterna, data: dataExterna});
+    emailExterno
+) => ({ nome: nomeExterno, senha: senhaExterna, data: dataExterna, email: emailExterno });
 
-function CadastrarUsuario(){
-    let usuario = USU(usu.value, sen.value, dat.value);
+// function CadastrarUsuario(){
+//     let usuario = USU(usu.value, sen.value, dat.value, email.value);
+//     usus.push(usuario);
+
+//     localStorage.setItem("usuarios_registrados", JSON.stringify(usus));
+
+//     window.location.href = "home.html"
+// };
+const formatoCampoSenha = {
+    {
+        mensagem: "A senha deve conter pelo menos uma letra maiuscula",
+        regra: /[A-Z]/
+    }
+    {
+        mensagem: "A senha deve conter pelo menos uma letra minuscula",
+        regra: /[a-z]/
+    }
+    {
+        mensagem: "A senha deve conter pelo menos um numero",
+        regra: /[0-9]/
+    }
+    {
+        mensagem: "A senha NÃO deve conter pelo menos um caractere especial",
+        regra: /[-_%$#@!.,;/=¨'"\|~^[]{}()*&ªº₢§¬£¢°/?<>:]/
+    }
+}
+function ValidarSenha() {
+    let senhaVerificada = sen.value;
+    let letrasMa = /[A-Z]/;
+    let letrasMi = /[a-z]/;
+    let numeros = /[0-9]/;
+    //     let caracterEsp = /[-_%$#@!.,;/=¨'"\|~^[]{}()*&ªº₢§¬£¢°/?]/;
+    let caractereEsp = /[ @#$%&* ]/;
+
+    if (letrasMa.test(senhaVerificada)
+        || letrasMi.test(senhaVerificada)
+        || numeros.test(senhaVerificada)
+        || caractereEsp.test(senhaVerificada) == false) {
+        console.log("OK!")
+    }
+    else {
+        console.log("Não OK")
+    }
+    }
+
+mail.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    let usuario = USU(usu.value, sen.value, dat.value, email.value);
     usus.push(usuario);
 
     localStorage.setItem("usuarios_registrados", JSON.stringify(usus));
 
     window.location.href = "home.html"
-};
+});
